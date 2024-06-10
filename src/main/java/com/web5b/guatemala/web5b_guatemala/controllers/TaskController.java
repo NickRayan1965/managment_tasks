3820492d51e86371f05eb43fa38b18c230664a03
NickRayan <nickcerron@gmail.com>
2024-06-10 00:28:24 -0500

    feat: Add CRUD operations for TaskController

--- src/main/java/com/web5b/guatemala/web5b_guatemala/controllers/TaskController.java
+++ src/main/java/com/web5b/guatemala/web5b_guatemala/controllers/TaskController.java
@@ -1,17 +1,23 @@
 package com.web5b.guatemala.web5b_guatemala.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web5b.guatemala.web5b_guatemala.dtos.create.CreateTaskDto;
 import com.web5b.guatemala.web5b_guatemala.dtos.res.TaskDto;
+import com.web5b.guatemala.web5b_guatemala.dtos.update.UpdateTaskDto;
 import com.web5b.guatemala.web5b_guatemala.services.TaskService;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -25,9 +31,25 @@ public class TaskController {
     return taskService.findOneById(id);
   }
 
+  @GetMapping
+  public Flux<TaskDto> findAll() {
+    return taskService.findAll();
+  }
+
   @PostMapping
   public Mono<TaskDto> create(@RequestBody CreateTaskDto dto) {
     return taskService.create(dto);
+  }
+
+  @PutMapping("/{id}")
+  public Mono<TaskDto> update(@PathVariable(name = "id") Long id, @RequestBody UpdateTaskDto dto) {
+    return taskService.update(id, dto);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(code = HttpStatus.NO_CONTENT)
+  public Mono<Void> delete(@PathVariable(name = "id") Long id) {
+    return taskService.delete(id);
   }
 
   
