fabfd5ccca5bbe3f32c11324a9efdb1db64061ae
NickRayan <nickcerron@gmail.com>
2024-06-10 00:43:27 -0500

    chore: Update ITaskRepository to include findAllEnabled method

--- src/main/java/com/web5b/guatemala/web5b_guatemala/repositories/ITaskRepository.java
+++ src/main/java/com/web5b/guatemala/web5b_guatemala/repositories/ITaskRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import com.web5b.guatemala.web5b_guatemala.dtos.db.TaskDbJoined;
 import com.web5b.guatemala.web5b_guatemala.entities.Task;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ITaskRepository extends R2dbcRepository<Task, Long>{
   @Query("select tasks.id as task_id, tasks.name as task_name, tasks.description as task_description, tasks.type_id as task_type_id, task_types.name as task_type_name, task_types.enabled as task_type_enabled, tasks.user_id as user_id, users.username as username, users.role as user_role, users.enabled as user_enabled, tasks.enabled as task_enabled from tasks left join task_types on tasks.type_id = task_types.id left join users on tasks.user_id = users.id where tasks.id = $1")
   Mono<TaskDbJoined> findOneById(Long id);
+
+  @Query("select tasks.id as task_id, tasks.name as task_name, tasks.description as task_description, tasks.type_id as task_type_id, task_types.name as task_type_name, task_types.enabled as task_type_enabled, tasks.user_id as user_id, users.username as username, users.role as user_role, users.enabled as user_enabled, tasks.enabled as task_enabled from tasks left join task_types on tasks.type_id = task_types.id left join users on tasks.user_id = users.id where tasks.enabled = true")
+  Flux<TaskDbJoined> findAllEnabled();
 }
--- src/main/java/com/web5b/guatemala/web5b_guatemala/services/TaskService.java
+++ src/main/java/com/web5b/guatemala/web5b_guatemala/services/TaskService.java
@@ -24,24 +24,23 @@ public class TaskService implements ITaskService {
   private final ITaskTypeService taskTypeService;
   private final ITaskMapper taskMapper;
 
+  @Override
+  public Flux<TaskDto> findAll() {
+    return taskRepository.findAllEnabled().map(taskMapper::toDto);
+  }
+
+  @Override
   public Mono<TaskDto> findOneById(Long id) {
     return taskRepository.findOneById(id)
         .map(taskMapper::toDto)
         .switchIfEmpty(Mono.error(new NotFoundException("Task not found.")));
   }
 
+  @Override
   public Mono<TaskDto> create(CreateTaskDto dto) {
     return getDtoVerified(taskMapper.dtoToEntity(dto))
         .flatMap(taskRepository::save)
         .map(taskMapper::toDto);
-  }
-
-  public Mono<Task> getDtoVerified(Task task) {
-    Flux<Mono<?>> resultFlux = Flux.fromIterable(Arrays.asList(
-        task.getUserId() != null ? userService.findOneById(task.getUserId()) : Mono.empty(),
-        task.getTypeId() != null ? taskTypeService.findOneById(task.getTypeId()) : Mono.empty()));
-    return Flux.zip(resultFlux, objects -> objects)
-        .then(Mono.just(task));
   }
 
   @Override
@@ -64,7 +63,11 @@ public class TaskService implements ITaskService {
   }
 
   @Override
-  public Flux<TaskDto> findAll() {
-    return taskRepository.findAll().map(taskMapper::toDto);
+  public Mono<Task> getDtoVerified(Task task) {
+    Flux<Mono<?>> resultFlux = Flux.fromIterable(Arrays.asList(
+        task.getUserId() != null ? userService.findOneById(task.getUserId()) : Mono.empty(),
+        task.getTypeId() != null ? taskTypeService.findOneById(task.getTypeId()) : Mono.empty()));
+    return Flux.zip(resultFlux, objects -> objects)
+        .then(Mono.just(task));
   }
 }
