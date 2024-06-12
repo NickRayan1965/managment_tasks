ea20ebcbe06737766c7af8464ff2b91891663af9
NickRayan <nickcerron@gmail.com>
2024-06-12 02:01:00 -0500

    feat: Add GetRequestUser annotation and resolver for retrieving user from request

--- src/main/java/com/web5b/guatemala/web5b_guatemala/controllers/AuthController.java
+++ src/main/java/com/web5b/guatemala/web5b_guatemala/controllers/AuthController.java
@@ -7,6 +7,8 @@ import com.web5b.guatemala.web5b_guatemala.dtos.req.create.CreateUserDto;
 import com.web5b.guatemala.web5b_guatemala.dtos.res.LoginResponseDto;
 import com.web5b.guatemala.web5b_guatemala.dtos.res.UserDto;
 import com.web5b.guatemala.web5b_guatemala.entities.Role;
+import com.web5b.guatemala.web5b_guatemala.entities.User;
+import com.web5b.guatemala.web5b_guatemala.security.decorators.GetRequestUser;
 import com.web5b.guatemala.web5b_guatemala.security.decorators.HasAuthority;
 import com.web5b.guatemala.web5b_guatemala.services.IAuthService;
 
@@ -26,14 +28,14 @@ public class AuthController {
   private final IAuthService authService;
 
   @PostMapping("/login")
-  public Mono<LoginResponseDto> login(@RequestBody LoginDto loginDto) {
+  public Mono<LoginResponseDto> login(@RequestBody LoginDto loginDto, @GetRequestUser User user) {
       return authService.login(loginDto);
   }
 
   @HasAuthority(Role.ADMIN)
   @PostMapping("/register")
-  public Mono<UserDto> register(@RequestBody CreateUserDto createUserDto, Authentication authentication) {
-      System.out.println(authentication);
+  public Mono<UserDto> register(@RequestBody CreateUserDto createUserDto, @GetRequestUser User user) {
+      System.out.println(user);
       return authService.register(createUserDto);
   }
   
---
+++ src/main/java/com/web5b/guatemala/web5b_guatemala/security/decorators/GetRequestUser.java
@@ -0,0 +1,11 @@
+package com.web5b.guatemala.web5b_guatemala.security.decorators;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GetRequestUser {
+}
