40ca5f192dae1c00cf5e2989cb7a7a87a9fe9f44
NickRayan <nickcerron@gmail.com>
2024-06-12 17:24:44 -0500

    feat: Refactor AuthController to remove unnecessary dependencies and update login and register methods

--- src/main/java/com/web5b/guatemala/web5b_guatemala/controllers/AuthController.java
+++ src/main/java/com/web5b/guatemala/web5b_guatemala/controllers/AuthController.java
@@ -15,7 +15,6 @@ import com.web5b.guatemala.web5b_guatemala.services.IAuthService;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,14 +27,13 @@ public class AuthController {
   private final IAuthService authService;
 
   @PostMapping("/login")
-  public Mono<LoginResponseDto> login(@RequestBody LoginDto loginDto, @GetRequestUser User user) {
+  public Mono<LoginResponseDto> login(@RequestBody LoginDto loginDto) {
       return authService.login(loginDto);
   }
 
   @HasAuthority(Role.ADMIN)
   @PostMapping("/register")
-  public Mono<UserDto> register(@RequestBody CreateUserDto createUserDto, @GetRequestUser User user) {
-      System.out.println(user);
+  public Mono<UserDto> register(@RequestBody CreateUserDto createUserDto) {
       return authService.register(createUserDto);
   }
   
