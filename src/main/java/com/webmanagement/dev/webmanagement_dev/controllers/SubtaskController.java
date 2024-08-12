0d488ec7a96711601fbdd85a1fd49bae4d60cbdb
NickRayan <nickcerron@gmail.com>
2024-08-11 22:01:31 -0500

    subtaskcontroller: update and delete by id add

--- src/main/java/com/webmanagement/dev/webmanagement_dev/controllers/SubtaskController.java
+++ src/main/java/com/webmanagement/dev/webmanagement_dev/controllers/SubtaskController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webmanagement.dev.webmanagement_dev.dtos.req.create.CreateSubtaskDto;
+import com.webmanagement.dev.webmanagement_dev.dtos.req.update.UpdateSubtaskDto;
 import com.webmanagement.dev.webmanagement_dev.entities.SubTask;
 import com.webmanagement.dev.webmanagement_dev.entities.User;
 import com.webmanagement.dev.webmanagement_dev.security.decorators.GetRequestUser;
@@ -15,7 +16,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,13 +31,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SubtaskController {
   private final ISubtaskService subtaskService;
 
-
   @PostMapping
   public Mono<SubTask> create(
-    @RequestParam("taskId") Long taskId,
-    @RequestBody @Valid CreateSubtaskDto dto,
-    @GetRequestUser User requester) {
+      @RequestParam() Long taskId,
+      @RequestBody @Valid CreateSubtaskDto dto,
+      @GetRequestUser User requester) {
     return subtaskService.create(dto, taskId, requester.getId());
   }
-  
+
+  @PutMapping("/{id}")
+  public Mono<SubTask> update(
+      @PathVariable() Long id,
+      @RequestBody @Valid UpdateSubtaskDto dto,
+      @GetRequestUser User requester) {
+    return subtaskService.updateById(id, dto, requester.getId());
+  }
+
+  @DeleteMapping("/{id}")
+  public Mono<Void> delete(
+      @PathVariable() Long id,
+      @GetRequestUser User requester) {
+    return subtaskService.delete(id, requester.getId());
+  }
+
 }
